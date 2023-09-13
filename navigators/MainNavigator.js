5e0f1e468f42ee4fbbe6a6d8b3a88524fd7e8bf7
ravi gupta <ravi2611gupta@gmail.com>
2023-09-13 16:28:38 +0530

    5️⃣  added a new screen

--- navigators/MainNavigator.js
+++ navigators/MainNavigator.js
@@ -3,9 +3,11 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Home from '../screens/Home';
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
 import AntDesign from 'react-native-vector-icons/AntDesign';
+import Feather from 'react-native-vector-icons/Feather';
 import Search from '../screens/Search';
 import Saved from '../screens/Saved';
 import Profile from '../screens/Profile';
+import Post from '../screens/Post';
 import { COLORS } from '../constants';
 
 function MainNavigator() {
@@ -36,6 +38,16 @@ function MainNavigator() {
                 options={{
                     tabBarIcon: ({ color }) => (
                         <AntDesign name='search1' color={color}
+                        size={25} />
+                    ),
+                }}
+            />
+            <Tab.Screen
+                name={'Post'}
+                component={Post}
+                options={{
+                    tabBarIcon: ({ color }) => (
+                        <Feather name='plus-square' color={color}
                         size={25} />
                     ),
                 }}
